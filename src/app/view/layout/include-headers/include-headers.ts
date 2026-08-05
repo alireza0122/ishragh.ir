0ce43646747365026a420f445a9a03dbfee3c567
alireza0122 <alirezamovassagh2@gmail.com>
2026-08-05 03:33:16 +0330

    1405/04/14

--- src/app/view/layout/include-headers/include-headers.ts
+++ src/app/view/layout/include-headers/include-headers.ts
@@ -6,6 +6,7 @@ type DataType = {
   href: string;
   icon: string;
 };
+
 @Component({
   selector: 'app-include-headers',
   standalone: true,
@@ -30,13 +31,26 @@ export class IncludeHeaders {
     { title: 'رزرو', href: '/visit', icon: '' },
     { title: 'درباره ما', href: '/abouts', icon: '' },
   ];
+  navItemsMobile: DataType[] = [
+    { title: '', href: '/', icon: '' },
+    { title: 'ثبت ایده', href: '/submit', icon: '' },
+    { title: 'محصولات', href: '/show', icon: '' },
+    { title: 'تیم ها', href: '/team', icon: '' },
+    { title: 'رزرو', href: '/visit', icon: '' },
+    { title: '', href: '/abouts', icon: '' },
+  ];
 
   toggleMenu(): void {
     this.isOpen = !this.isOpen;
   }
+  isSubMenuOpen = false;
 
+  toggleSubMenu(): void {
+    this.isSubMenuOpen = !this.isSubMenuOpen;
+  }
   closeMenu(): void {
     this.isOpen = false;
+    this.isSubMenuOpen = false;
   }
 
   @HostListener('window:scroll')
